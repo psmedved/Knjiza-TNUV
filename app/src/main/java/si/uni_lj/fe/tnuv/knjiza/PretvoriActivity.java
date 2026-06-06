b32cc1021d3ab40a2f36f4b041797e20cdb504d6
psmedved <ps3900@student.uni-lj.si>
2026-06-06 22:31:51 +0200

    Popravi v PretvoriA - dodajanje gumba za ponovno slikanje. Ikona za aplikacijo

--- app/src/main/java/si/uni_lj/fe/tnuv/knjiza/PretvoriActivity.java
+++ app/src/main/java/si/uni_lj/fe/tnuv/knjiza/PretvoriActivity.java
@@ -44,6 +44,7 @@ public class PretvoriActivity extends MainActivity {
         bottomAppMenu.setOnItemSelectedListener(this::obKlikuSpodnjeNavigacijskeVrstice);
         bottomAppMenu.getMenu().findItem(R.id.btn_n_naprej).setEnabled(true);
 
+        findViewById(R.id.btn_p_ponovi).setOnClickListener(v -> finish());
         findViewById(R.id.btn_p_nadaljuj).setOnClickListener(v -> {
             if(pretvorjenoBesedilo.trim().isEmpty()) {
                 obvestiloONapaki(getString(R.string.obvestilo_sporocilo_napaka_citat_prazno_izberi));
